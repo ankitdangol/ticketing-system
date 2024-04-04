59ffcec239181bfb8d419a48964b2be2b6ae3bbf
ankitdangol <98317675+ankitdangol@users.noreply.github.com>
2024-04-04 12:05:26 +0545

    Update CallbackHandler.tsx

--- src/components/CallbackHandler.tsx
+++ src/components/CallbackHandler.tsx
@@ -4,7 +4,7 @@ import HomePage from '../pages/HomePage';
 const CallbackHandler = () => {
     const url = window.location;
     const access_token = new URLSearchParams(url.search).get('access_token') ?? "";
-
+console.log(hello)
     useEffect(() => {
         localStorage.setItem('access_token', access_token ?? "");
     }, [access_token]);
